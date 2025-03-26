4bc7cacfc3fde7da3f533f40f2d77f8b93b2ad74
Lucas Zarate <zaratelucasmartin1@gmail.com>
2025-03-26 16:30:53 -0300

    comente

--- ejercicios.py
+++ ejercicios.py
@@ -1,4 +1,7 @@
+#se define variable nombre
 nombre = "lucas"
+#se define vairble materia
 apellido = "Zarate"
 materia = "programacion 1"
+#se imprime mensaje por consola
 print(f"Hola {nombre}  {apellido}, estamos probando para {materia} ")
